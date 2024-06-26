47d6c340bd6f9065c5c570908e5d0fe4f110a493
williamjs95 <williamj.silva@live.com>
2024-06-25 23:36:55 -0300

    chore: Add header component and global CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post 
         author="William Silva" 
         content="fkdakjaçl ajkdaçfassdjk ç adkfjkadkça dkjfdkça dasjfkaçdjskasfad" 
