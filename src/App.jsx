93cfd676ec5c9c5e36e2701ded0eb83ff3bb86dd
williamjs95 <williamj.silva@live.com>
2024-06-26 15:13:40 -0300

    chore: Update dependencies and add sidebar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="William Silva" 
-        content="fkdakjaçl ajkdaçfassdjk ç adkfjkadkça dkjfdkça dasjfkaçdjskasfad" 
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="lorekkf afjkdaç kaldfjk hfhglça"
-      />
+      <div className={styles.wrpper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="William Silva"
+            content="fkdakjaçl ajkdaçfassdjk ç adkfjkadkça dkjfdkça dasjfkaçdjskasfad"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="lorekkf afjkdaç kaldfjk hfhglça"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src='https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+      />
+
+      <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src='https://avatars.githubusercontent.com/u/129805046?v=4' 
+        />
+
+        <strong>William Silva</strong>
+        <spam>Web Developer</spam>
+      </div>
+
+      <footer>
+        <a href='#'>
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
