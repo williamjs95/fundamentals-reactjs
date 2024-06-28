997f34cb2ad5a848fbd5cbed38520b90370ce459
williamjs95 <williamj.silva@live.com>
2024-06-28 20:22:00 -0300

    refactor: Add onDeleteComment prop to Comment component and implement delete functionality in Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,6 +31,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete
+    })
+
+    setComments(commentsWithoutDeletedOne)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -49,11 +57,11 @@ export function Post({ author, publishedAt, content }) {
       </header>
 
       <div className={styles.content}>
-        {content.map((line, index) => {
+        {content.map((line) => {
           if (line.type === 'paragraph') {
-            return <p key={index}>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p key={index}><a href='#'>{line.content}</a></p>
+            return <p key={line.content}><a href='#'>{line.content}</a></p>
           }
         })}
       </div>
@@ -74,7 +82,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment 
+              key={comment} 
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          ) 
         })}
       </div>
     </article>
